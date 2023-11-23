9fc1466dabe1b27cf9fb4526a67b0645dadd5169
Thorsten Schlüter <thorstenschluter@dyn-17ab.mip.uni-hannover.de>
2023-11-23 12:47:13 +0100

    Updates

--- precache-manifest.d667b66c784644cc7dac066f5e767da8.js
+++ precache-manifest.d667b66c784644cc7dac066f5e767da8.js
@@ -1,27 +1,27 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "5adc41eaea2fcba4c78027a5751cd000",
+    "revision": "c094544b60859bbb301ca559a723e075",
     "url": "/LyckaWebshop/index.html"
   },
   {
-    "revision": "d3665ddd506b136823c3",
+    "revision": "f4192a13b498b7fd8b0b",
     "url": "/LyckaWebshop/static/css/main.f250ab75.chunk.css"
   },
   {
-    "revision": "ff975f1488997419219a",
-    "url": "/LyckaWebshop/static/js/2.efa4adfd.chunk.js"
+    "revision": "234a8e448feeda53914e",
+    "url": "/LyckaWebshop/static/js/2.d6f34416.chunk.js"
   },
   {
     "revision": "72f265bfc6b778ed03ef626d21aac8dd",
-    "url": "/LyckaWebshop/static/js/2.efa4adfd.chunk.js.LICENSE.txt"
+    "url": "/LyckaWebshop/static/js/2.d6f34416.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "d3665ddd506b136823c3",
-    "url": "/LyckaWebshop/static/js/main.796f05a8.chunk.js"
+    "revision": "f4192a13b498b7fd8b0b",
+    "url": "/LyckaWebshop/static/js/main.8163a995.chunk.js"
   },
   {
     "revision": "4e0e34f265fae8f33b01b27ae29d9d6f",
-    "url": "/LyckaWebshop/static/js/main.796f05a8.chunk.js.LICENSE.txt"
+    "url": "/LyckaWebshop/static/js/main.8163a995.chunk.js.LICENSE.txt"
   },
   {
     "revision": "c4475f1fd66af7c8bd4e",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/LyckaWebshop/precache-manifest.d459e8f57cbc8f7935702e7b755e59ef.js"
+  "/LyckaWebshop/precache-manifest.d667b66c784644cc7dac066f5e767da8.js"
 );
 
 self.addEventListener('message', (event) => {
